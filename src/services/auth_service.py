21ec5f2774074a98ed4ef85bd59e9dd36b9ac91d
davimcruz <davimachadocruz@gmail.com>
2024-11-28 04:07:25 -0300

    feat(connection): adicionar banco de dados em memória para testing no connection diretamente

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -6,16 +6,27 @@ from fastapi.security import OAuth2PasswordBearer
 from uuid import UUID
 import os
 from dotenv import load_dotenv
+from passlib.context import CryptContext
 
 load_dotenv()
 
-# Configurações do JWT
-SECRET_KEY = os.getenv("SECRET_KEY") 
+# Configurações de autenticação
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Usa uma chave de teste quando estiver em ambiente de teste (pra facilitar o CI)
+TESTING = os.getenv("TESTING", "False").lower() == "true"
+SECRET_KEY = "test_secret_key" if TESTING else os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # Configura o schema de autenticação OAuth2 (tokenUrl é o endpoint pra login)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/login")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     # Cria um token de acesso JWT
